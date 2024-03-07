c1ff351fb6ec9090954966a1d8cae5604af42bd3
doridoro <dorothea.reher@gmail.com>
2024-03-07 16:49:22 +0100

    move files into new app. refer url new.

--- lettings/views.py
+++ lettings/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+from oc_lettings_site.models import Letting  # TODO: change after migration
+
+
+def lettings_index(request):
+    lettings_list = Letting.objects.all()
+    context = {'lettings_list': lettings_list}
+    return render(request, 'lettings_index.html', context)
+
+
+def letting(request, letting_id):
+    single_letting = Letting.objects.get(id=letting_id)
+    context = {
+        'title': single_letting.title,
+        'address': single_letting.address,
+    }
+    return render(request, 'letting.html', context)
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from oc_lettings_site.models import Profile  # TODO: change after migration
+
+
+def profiles_index(request):
+    profiles_list = Profile.objects.all()
+    context = {'profiles_list': profiles_list}
+    return render(request, 'profiles_index.html', context)
+
+
+def profile(request, username):
+    single_profile = Profile.objects.get(user__username=username)
+    context = {'profile': single_profile}
+    return render(request, 'profile.html', context)
