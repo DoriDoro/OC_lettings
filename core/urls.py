0a82bfb06338f3b8b1f8d978dbf03c11209bd5f1
doridoro <dorothea.reher@gmail.com>
2024-03-13 16:23:39 +0100

    create 404 and 500 templates and views. add handler to urls

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,9 @@ from core.views import index
 
 app_name = 'core'
 
+handler404 = 'core.views.error_404'
+handler500 = 'core.views.error_500'
+
 urlpatterns = [
     path('', index, name='index'),
 ]
