24a85f876ba6f4d280298c0f6710d7e9091f1476
doridoro <dorothea.reher@gmail.com>
2024-05-02 15:32:29 +0200

    add database name back to settings

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -70,7 +70,7 @@ WSGI_APPLICATION = 'oc_lettings_site.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, os.environ.get('NAME')),
+        'NAME': os.path.join(BASE_DIR, 'oc-lettings-site.sqlite3'),
     }
 }
 
