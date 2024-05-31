4357ef2716e42ca6507bd484ab47f4d9954bae9e
doridoro <dorothea.reher@gmail.com>
2024-05-31 14:33:28 +0200

    modified docstring

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -14,7 +14,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# set default to False
 DEBUG = os.environ.get("DEBUG", "False")
 
 # set an empty list as default
